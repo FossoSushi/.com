9fd1672f2cd007acfa7b55b7d27a0b9cd29be9a9
Микола Кондратюк <95100219+Mykola28112002@users.noreply.github.com>
2024-04-13 06:05:00 +0200

    1

--- src/components/FilterMenu/FilterMenu.jsx
+++ src/components/FilterMenu/FilterMenu.jsx
@@ -1,11 +1,9 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import css from './styles.module.scss';
 import { Delivery } from '../../assets/svgComponents/Delivery';
 
 const FilterMenu = () => {
     const buttonRef = useRef(null);
-    const filterRef = useRef(null);
-    const [isSticky, setIsSticky] = useState(false);
     const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
         if (section) {
@@ -17,39 +15,12 @@ const FilterMenu = () => {
             buttonRef.current.focus();
         }
     }, []);
+
     
-    useEffect(() => {
-        const handleScroll = () => {
-        if (filterRef.current) {
-            const { top } = filterRef.current.getBoundingClientRect();
-            setIsSticky(top <= 0);
-        }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-        window.removeEventListener('scroll', handleScroll);
-        };
-        }, []);
-
-    useEffect(() => {
-        const handleScrollUp = () => {
-        if (isSticky && window.scrollY === 0) {
-            setIsSticky(false);
-        }
-        };
-
-        window.addEventListener('scroll', handleScrollUp);
-
-        return () => {
-        window.removeEventListener('scroll', handleScrollUp);
-        };
-    }, [isSticky]);
     
     return (
         <div className={css.FilterMenu} id='DELIVERY'>
-            <div className={isSticky ? `${css.Filter} ${css.Sticky}` : css.Filter} ref={filterRef}>
+            <div className={css.Filter}>
                 <button ref={buttonRef} onClick={() => scrollToSection('DELIVERY')}><Delivery />DELIVERY</button>
                 <button onClick={() => scrollToSection('SETS')}>SETS</button>
                 <button onClick={() => scrollToSection('SUSHI')}>SUSHI</button>
