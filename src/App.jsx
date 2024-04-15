f3d68ca0e5b900137af7de3479ffe5c3b2cff49d
Микола Кондратюк <95100219+Mykola28112002@users.noreply.github.com>
2024-04-15 20:53:52 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,26 @@ import css from './styles.module.scss';
 import React from 'react';
 import HomePage from './pages/HomePage';
 import MenuPage from './pages/MenuPage';
-import { GlobalStateProvider } from './GlobalStateContext/GlobalStateContext';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutUs from './pages/AboutUsPage';
 import Contact from './pages/ContactPage';
 import Order from './pages/OrderPage';
 
+
 const App = () => {
   return (
-    <GlobalStateProvider>
-      <div className={css.app}>
-        <BrowserRouter basename='/Sushi-Alberta'>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/menu" element={<MenuPage />} />
+    <div className={css.app}>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<HomePage />} />
             <Route path="/about" element={<AboutUs />} />
+            <Route path="/menu" element={<MenuPage />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/order" element={<Order />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </GlobalStateProvider>
-    );
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStateProvider } from './GlobalStateContext/GlobalStateContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename='/Sushi-Alberta'>
       {/* <Provider store={store}>
           <ThemeProvider theme={theme}> */}
-            <App/>
+      <GlobalStateProvider>
+        <App/>
+      </GlobalStateProvider>
           {/* </ThemeProvider>
       </Provider> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
